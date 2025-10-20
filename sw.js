c6a2c9d5473a32c4ac70a1ce940fb5174f7d2aa4
Luna447 <m.boy.info@gmail.com>
2025-10-20 19:19:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 // sw.js
-const CACHE = 'veganscanner-v14';
+const CACHE = 'veganscanner-v15';
 const ASSETS = [
   './','./index.html','./app.js','./manifest.webmanifest','./ingredients-data.json',
   './icons/icon-192.png','./icons/icon-512.png',
   './tesseract/tesseract.min.js','./tesseract/worker.min.js',
-  './tesseract/tesseract-core.wasm.js',   // jetzt 2.1.1
-  './tesseract/tesseract-core.wasm',      // jetzt 2.1.1
+  './tesseract/tesseract-core.wasm.js','./tesseract/tesseract-core.wasm',
   './tesseract/tessdata/eng.traineddata.gz','./tesseract/tessdata/deu.traineddata.gz'
 ];
+
 
 
 

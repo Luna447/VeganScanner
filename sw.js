f58c82d981dada99f4b3c3c96d0db4945e2cb6b2
Luna447 <m.boy.info@gmail.com>
2025-10-20 14:13:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE = 'veganscanner-v6';
+const CACHE = 'veganscanner-v7';
 const ASSETS = [
   './','./index.html','./app.js','./manifest.webmanifest','./ingredients-data.json',
   './icons/icon-192.png','./icons/icon-512.png',
   './tesseract/tesseract.min.js','./tesseract/worker.min.js','./tesseract/tesseract-core.wasm',
   './tesseract/tessdata/eng.traineddata.gz','./tesseract/tessdata/deu.traineddata.gz'
 ];
+
 
 
 self.addEventListener('install', e => {

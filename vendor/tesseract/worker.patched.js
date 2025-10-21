37d3fe916152d56f38184e90034c0cc8f0b3dc31
Luna447 <m.boy.info@gmail.com>
2025-10-21 13:07:20 +0200

    Add worker.patched.js to fix importScripts bug

---
+++ vendor/tesseract/worker.patched.js
@@ -0,0 +1,3 @@
+/* worker.patched.js — zwingt einen sauberen corePath bevor der Minified-Worker läuft */
+self.version = '/VeganScanner/vendor/tesseract/tesseract-core.wasm.js';
+importScripts('/VeganScanner/vendor/tesseract/worker.min.js');
